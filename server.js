18fa390690100f5ecf5d177c3f8d099af6c79872
me-key-0 <me.key.as.24@gmail.com>
2024-08-13 09:45:02 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { usersRoute } = require("./routes/usersRoute");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const connectDb = require("./config/db");
+app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
+
+connectDb();
+app.use(cors());
+app.use(express.json());
+app.use("/users", require("./routes/usersRoute"));
+app.use("/places", require("./routes/placesRoute"));
