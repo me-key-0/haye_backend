23d969497b98a887d20f6b519e4b7d44fd79d7a2
marya <mariamawittesfaye866@gmail.com>
2024-08-13 10:16:01 -0400

    event model,controller and routes done

--- server.js
+++ server.js
@@ -19,3 +19,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", require("./routes/usersRoute"));
 app.use("/places", require("./routes/placesRoute"));
+app.use("/events", require("./routes/eventsRoute"));
