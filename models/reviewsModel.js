d51425682b00d7a1b5a286db4f13be7fe0fdd6da
marya <mariamawittesfaye866@gmail.com>
2024-08-13 11:46:25 -0400

    review model done

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -18,28 +18,11 @@ const ReviewSchema = new mongoose.Schema({
      },
   comment: {
      type: String,
-      required: true 
-    },
-  date: { 
-    type: Date, 
-    default: Date.now 
-},
-  title: { 
-    type: String, 
-    default: '' 
-},
-  anonymous: { 
-    type: Boolean, 
-    default: false
- },
-  verified_purchase: { 
-    type: Boolean,
-     default: false 
-    },
-  helpful_count: {
-     type: Number,
-      default: 0 
+      required: false 
     }
+}
+,{
+    timestamps: true
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);
