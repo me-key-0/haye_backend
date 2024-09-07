33b8c30f7fad38db85f080b7768318530fc34dfe
me-key-0 <121063294+me-key-0@users.noreply.github.com>
2024-09-07 11:39:42 +0300

    Update server.js cors origin

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const server = app.listen(port, () => {
 connectDb();
 app.use(
   cors({
-    origin: "http://hayeplaces.vercel.app/",
+    origin: "https://hayeplaces.vercel.app/",
   })
 );
 app.use(express.json());
